a2f4d70835f982b481b86da826485bb208df2a9d
rmascuse <jameson.w7281@gmail.com>
2023-05-31 10:16:11 -0600

    home page

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import '../css/Home.css'
+import photo1 from '../imgs/headshot.jpg'
 
 function Home() {
   return (
-    <div>
-      <h1>Welcome to My Website</h1>
-      <p>This is the home page of my website.</p>
+    <div className='homebody'>
+      <h1>Welcome to My Portfolio</h1>
+      <img id='headshot' src={photo1} alt='Head shot'/>
+      <div id='txt'>
+        <p>Hello! My name is Jameson Walter. I am a student at Colorado State University. I hope through navigating through my website you can learn more about me.</p>
+      </div>
     </div>
   );
 }
