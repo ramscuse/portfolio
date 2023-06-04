1ce8ee3ab7375d8264c61060b13d356b9675ba1e
rmascuse <jameson.w7281@gmail.com>
2023-06-04 17:15:43 -0600

    cant have that

--- src/index.js
+++ src/index.js
@@ -4,10 +4,10 @@ import './css/index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from "react-router-dom";
-import { Amplify } from 'aws-amplify';
-import awsExports from './aws-exports';
+// import { Amplify } from 'aws-amplify';
+// import awsExports from './aws-exports';
 
-Amplify.configure(awsExports);
+// Amplify.configure(awsExports);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
