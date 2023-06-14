4d9fd5f9400324e2efd42b0a81ec4af31fd2b0dd
rmascuse <jameson.w7281@gmail.com>
2023-06-13 21:00:48 -0600

    Added Pictures to Page

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import '../css/Project.css';
-
-import { Storage } from "aws-amplify";
+import Schedule from '../imgs/Schedule.png'
+import Search from '../imgs/Search.png'
+import list from '../imgs/list.png'
+import menu from '../imgs/menu.png'
+import select from '../imgs/select.png'
+import game from '../imgs/game.png'
+import color from '../imgs/color.png'
 
 function Project() {
   return (
     <div className='projectBody'>
       <div id='trip'>
         <h2>Trip Itinerary</h2>
+        <img id='Schedule' src={Schedule} alt='Itinerary Home'/>
+        <img id='Search' src={Search} alt='Itinerary Search'/>
+        <img id='List' src={list} alt='Itinerary List'/>
         <a href='https://github.com/ramscuse/cs314' target='_blank' rel="noopener noreferrer">
           <button>Check out the Code</button>
         </a>
@@ -26,6 +34,7 @@ function Project() {
       </div>
       <div id='color'>
         <h2>Color Coordinate Website</h2>
+        <img id='color' src={color} alt='Color Coordinate Website'/>
         <a href="https://github.com/ramscuse/school/tree/master/local_html/m2" target='_blank' rel="noopener noreferrer">
           <button>Check out the Code</button>
         </a>
@@ -35,6 +44,9 @@ function Project() {
       </div>
       <div id='tron'>
         <h2>Tron Game</h2>
+        <img id='menu' src={menu} alt='Game Menu'/>
+        <img id='select' src={select} alt='Game Select'/>
+        <img id='game' src={game} alt='Game'/>
         <a href='https://github.com/ramscuse/TronGame/tree/main' target='_blank' rel="noopener noreferrer">
           <button>Check out the Code</button>
         </a>
