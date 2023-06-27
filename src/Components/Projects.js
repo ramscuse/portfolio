24c9233582cf19f39c7826c163e75bfb624688ce
ramscuse <jameson.w7281@gmail.com>
2023-06-27 13:53:05 -0600

    More details.

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -43,7 +43,7 @@ function Project() {
           This is the website you are currently browser! I hope you are enjoying it so far.
         </p>
         <p>
-          I built this website from the group up using React for the front-end and AWS for the back-end. The code is being hosted on github 
+          I built this website from the ground up using React for the front-end and AWS for the back-end. The code is being hosted on github 
           and deployed using AWS.
         </p>
         <a href='https://github.com/ramscuse/portfolio' target='_blank' rel="noopener noreferrer">
@@ -56,13 +56,20 @@ function Project() {
           During my internship, my most notable project was to build a blank but functional store front small business could fill in with
           their product. Some of my code was used to create the Lima's Coffee website.
         </p>
-        <a href='https://www.limacoffeeroasters.com/collections/coffee' target='_blank' rel="noopener noreferrer">Lima's Website</a>
+        <a href='https://www.limacoffeeroasters.com/collections/coffee' target='_blank' rel="noopener noreferrer">
+          <button>Lima's Website</button>
+        </a>
         <a href="https://github.com/ramscuse/project2" target='_blank' rel="noopener noreferrer">
           <button>Check out the Code</button>
         </a>
       </div>
       <div id='color'>
         <h2>Color Coordinate Website</h2>
+        <p>
+          This project was for another one of my classes. I built a website from scratch using PHP, SQL, JavaScript, HTML, CSS, and fuel. 
+          It uses PHP for the framework and JavaScript for the color painter. It also uses a SQL database that the user can add or substract 
+          colors from.
+        </p>
         <img id='color' src={color} alt='Color Coordinate Website'/>
         <a href="https://github.com/ramscuse/school/tree/master/local_html/m2" target='_blank' rel="noopener noreferrer">
           <button>Check out the Code</button>
@@ -70,9 +77,17 @@ function Project() {
       </div>
       <div id='deter'>
         <h2>DETER Projects</h2>
+        <p>
+          I used DETER in my systems secutities class. We used it to create and defend against several different DOS attacks. Most notable being 
+          an SQL injection attack.
+        </p>
       </div>
       <div id='tron'>
         <h2>Tron Game</h2>
+        <p>
+          I created this tron game in High School for fun. It was made completely out of Java. I had to learn how to build collion detection and 
+          how to render sprites in javafx. I also built a very simple AI model that would play against the player.
+        </p>
         <img id='menu' src={menu} alt='Game Menu'/>
         <img id='select' src={select} alt='Game Select'/>
         <img id='game' src={game} alt='Game'/>
@@ -82,6 +97,10 @@ function Project() {
       </div>
       <div id='schedule'>
         <h2>TV Show Schedule</h2>
+        <p>
+          I have built a lot of tools using C++. The TV Show Schedule shows off most of my skills in C++, but not everything. You can check out 
+          more of my C++ projects on my gitHub.
+        </p>
         <a href='https://github.com/ramscuse/school/tree/master/cs253/hw6' target='_blank' rel="noopener noreferrer">
           <button>Check out the Code</button>
         </a>
