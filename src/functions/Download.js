b4e7f661afdb16e502bb844b2895f8e66aa93e7e
ramscuse <jameson.w7281@gmail.com>
2026-05-28 23:26:23 -0600

    Update portfolio content

--- src/functions/Download.js
+++ src/functions/Download.js
@@ -1,6 +1,10 @@
-export function download(filename) {
+export function download(fileOrUrl) {
+  const isAbsolute = /^https?:\/\//.test(fileOrUrl);
   const a = document.createElement('a');
-  a.href = `/${filename}`;
-  a.download = filename;
+  a.href = isAbsolute ? fileOrUrl : `/${fileOrUrl}`;
+  // The `download` attribute is honored for same-origin files; cross-origin
+  // (e.g. Vercel Blob) relies on the `?download=1` query param on the URL.
+  if (!isAbsolute) a.download = fileOrUrl;
+  a.rel = 'noopener';
   a.click();
 }
